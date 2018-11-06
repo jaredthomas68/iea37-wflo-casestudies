9ae1126eee2bc92c6ebaf2d0adc0ada9eab371ef
Jared J. Thomas <jaredthomas68@gmail.com>
2018-11-06 10:04:38 -0700

    corrected bash script for iea case 2 runs

--- case2/optimizations/no_then_yes_local_ti/opt_snopt_relax.sh
+++ case2/optimizations/no_then_yes_local_ti/opt_snopt_relax.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --time=15:00:00   # walltime
-#SBATCH --ntasks=6   # number of processor cores per sub-job(i.e. tasks)
-#SBATCH --mem-per-cpu=500G  # memory per CPU core
-#SBATCH -J "38 turbs snopt relax"   # job name
+#SBATCH --ntasks=4   # number of processor cores per sub-job(i.e. tasks)
+#SBATCH --mem-per-cpu=500M  # memory per CPU core
+#SBATCH -J "9 turbs snopt relax for iea37 case 2"   # job name
 #SBATCH --mail-user=jaredthomas68@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
